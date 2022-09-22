4be968f161c783af860f2ed0e5670194e23712fa
vbs-karthikSuresh <112486804+vbs-karthikSuresh@users.noreply.github.com>
2022-09-22 19:23:59 +0530

    Form Authentication to add employees done on 21 and 22/09/2022

---
+++ FormAuthentication/FormAuthentication/Controllers/EmployeeController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FormAuthentication;
+
+namespace FormAuthentication.Controllers
+{
+    [Authorize]
+    public class EmployeeController : Controller
+    {
+        private Freshers_Training2022Entities db = new Freshers_Training2022Entities();
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var form_Employee = db.Form_Employee.Include(f => f.Form_Department);
+            return View(form_Employee.ToList());
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Form_Employee form_Employee = db.Form_Employee.Find(id);
+            if (form_Employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(form_Employee);
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            ViewBag.DepartmentId = new SelectList(db.Form_Department, "Id", "DepartmentName");
+            return View();
+        }
+
+        // POST: Employee/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Gender,Age,Position,Office,HireDate,Salary,DepartmentId")] Form_Employee form_Employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Form_Employee.Add(form_Employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DepartmentId = new SelectList(db.Form_Department, "Id", "DepartmentName", form_Employee.DepartmentId);
+            return View(form_Employee);
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Form_Employee form_Employee = db.Form_Employee.Find(id);
+            if (form_Employee == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DepartmentId = new SelectList(db.Form_Department, "Id", "DepartmentName", form_Employee.DepartmentId);
+            return View(form_Employee);
+        }
+
+        // POST: Employee/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Gender,Age,Position,Office,HireDate,Salary,DepartmentId")] Form_Employee form_Employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(form_Employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DepartmentId = new SelectList(db.Form_Department, "Id", "DepartmentName", form_Employee.DepartmentId);
+            return View(form_Employee);
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Form_Employee form_Employee = db.Form_Employee.Find(id);
+            if (form_Employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(form_Employee);
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Form_Employee form_Employee = db.Form_Employee.Find(id);
+            db.Form_Employee.Remove(form_Employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FormAuthentication/FormAuthentication/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace FormAuthentication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Freshers_Training2022Entities dbContext = new Freshers_Training2022Entities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Form_Users user)
+        {
+            if (ModelState.IsValid)
+            {
+                bool isValidUser = dbContext.Form_Users.Any(x => x.Username.ToLower() == user.Username.ToLower() && x.Password == user.Password);
+                if (isValidUser)
+                {
+                    FormsAuthentication.SetAuthCookie(user.Username, false);
+                    return RedirectToAction("Index", "Employee");
+                }
+            }
+            ModelState.AddModelError(" ", "INVALID USERNAME OR PASSWORD");
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Form_Users registerUser)
+        {
+            if (ModelState.IsValid)
+            {
+                dbContext.Form_Users.Add(registerUser);
+                dbContext.SaveChanges();
+                return RedirectToAction("Login");
+            }            
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        } 
+    }
+}
